054a03526b53ff6c63de4c51487be26f5daa184e
PREM-KUMAR-D <premkumar88845@gmail.com>
2024-11-14 00:15:53 +0530

    revision project validating user inputs

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,20 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./AddUser.css";
 
 const AddUser = () => {
+
+  const [userName, setUserName] = useState("");
+
+  const [age, setAge] = useState("");
+
+  const nameHandler = (event) => {
+    setUserName(event.target.value); 
+  }
+
+  const ageHandler = (event) => {
+    setAge(event.target.value)
+  };
+
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(userName,age);
+    
+    setUserName("");
+    setAge("");
   };
 
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={nameHandler} value={userName} />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" />
+        <input id="age" type="number"
+          onChange={ageHandler} value={age}/>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
